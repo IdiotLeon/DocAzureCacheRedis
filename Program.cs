f4d681617c6fc2cb823d1615cb1efcbd28166f31
Leon <yanglyu.leon.7@gmail.com>
2019-12-25 16:49:09 -0800

    start: Replicating what the doc tells

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+
+namespace CoreAzureCacheRedis
+{
+    class Program
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+        const string SecretName = "CacheConnection";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+            Configuration = builder.Build();
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = Configuration[SecretName];
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
